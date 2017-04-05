458523b02921f31846a50f0e7bdc0c1bb6eeacc9
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-04-04 22:23:32 -0700

    add mastodon-width

---
+++ mastodon-width.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Mastodon column width
+// @description  Expands the width of columns on Mastodon.social and Mastodon.xyz. For other instances, you're on your own.
+// @author       Stephen Jennings
+// @namespace    http://jennings.io/
+// @version      1.20170404
+// @match        https://mastodon.social/web/*
+// @match        https://mastodon.xyz/web/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  GM_addStyle('.column {' +
+      'flex: 1 0 330px' +
+    '}');
+
+})();
