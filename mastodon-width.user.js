039a7eb1c975d666659542594f54b76c3d465bd9
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-04-04 22:36:53 -0700

    fix mastodon-width in Greasemonkey
    Tampermonkey is fine with @run-at document-start, but Greasemonkey
    isn't.
    
    If I remove @run-at, Tampermonkey then flashes an unstyled page, which
    is ugly.
    
    Tampermonkey supports document-body, which avoids the flash, and
    Greasemonkey ignores it, so this seems a good workaround.

--- mastodon-width.user.js
+++ mastodon-width.user.js
@@ -3,11 +3,11 @@
 // @description  Expands the width of columns on Mastodon.social and Mastodon.xyz. For other instances, you're on your own.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20170404
+// @version      1.20170404.1
 // @match        https://mastodon.social/web/*
 // @match        https://mastodon.xyz/web/*
 // @grant        GM_addStyle
-// @run-at       document-start
+// @run-at       document-body
 // ==/UserScript==
 
 (function() {
