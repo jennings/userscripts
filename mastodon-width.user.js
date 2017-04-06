96b6775ee9cdeb5871e93e690a245585b3160faa
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-04-05 19:28:13 -0700

    match against more Mastodon instances

--- mastodon-width.user.js
+++ mastodon-width.user.js
@@ -1,15 +1,21 @@
 // ==UserScript==
 // @name         Mastodon column width
-// @description  Expands the width of columns on Mastodon.social and Mastodon.xyz. For other instances, you're on your own.
+// @description  Expands the width of columns on some of the popular Mastodon instances.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20170405
+// @version      1.20170405.1
+// @grant        GM_addStyle
+// @run-at       document-body
 // @match        https://mastodon.social/
 // @match        https://mastodon.social/web/*
 // @match        https://mastodon.xyz/
 // @match        https://mastodon.xyz/web/*
-// @grant        GM_addStyle
-// @run-at       document-body
+// @match        https://mastodon.cloud/
+// @match        https://mastodon.cloud/web/*
+// @match        https://social.tchncs.de/
+// @match        https://social.tchncs.de/web/*
+// @match        https://social.targaryen.house/
+// @match        https://social.targaryen.house/web/*
 // ==/UserScript==
 
 (function() {
