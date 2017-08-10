c139ab70ce5b6c76261cae879199f935cd263271
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-08-09 23:52:02 -0700

    github-rainbow: make the search text legible

--- github-rainbow.user.js
+++ github-rainbow.user.js
@@ -3,7 +3,7 @@
 // @description  Makes the GitHub menu bar more fabulous.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20170309
+// @version      1.20170809
 // @match        https://github.com/*
 // @grant        GM_addStyle
 // @run-at       document-start
@@ -14,20 +14,42 @@
 (function() {
   'use strict';
 
-  GM_addStyle('.header {' +
-      'background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);' +
-      'padding-top: 3px;' +
-      'padding-bottom: 3px;' +
-    '}');
+  GM_addStyle(''
+    + '.header {'
+    +   'background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);'
+    +   'padding-top: 3px;'
+    +   'padding-bottom: 3px;'
+    + '}'
 
-  GM_addStyle('.header .container {' +
-    'background-color: rgba(100, 100, 100, 0.5);' +
-    'border-radius: 15px;' +
-    'padding: 5px 10px;' +
-  '}');
+    + '.header .container {'
+    +   'background-color: rgba(100, 100, 100, 0.5);'
+    +   'border-radius: 15px;'
+    +   'padding: 5px 10px;'
+    + '}'
 
-  GM_addStyle('.header .header-search-scope {' +
-    'border-right-color: rgba(255, 255, 255, 0.75);' +
-  '}');
+    + '.header .header-search, .header .header-search-input {'
+    +   'color: white;'
+    +   'text-shadow: 1px 1px 1px black;'
+    + '}'
+
+    + '.header .header-search-wrapper, .header .header-search-wrapper.focus {'
+    +   'background-color: rgba(0, 0, 0, 0.15);'
+    + '}'
+
+    + '.header .header-search, .header .header-search-input {'
+    +   'color: white;'
+    +   'text-shadow: 1px 1px 1px black;'
+    + '}'
+
+    + '.header .header-search-scope, .header .header-search-wrapper.focus .header-search-scope {'
+    +   'color: white;'
+    +   'border-right-color: rgba(255, 255, 255, 0.75);'
+    + '}'
+
+    + '.header .header-navlink {'
+    +   'color: white;'
+    +   'text-shadow: 1px 1px 1px black;'
+    + '}'
+  );
 
 })();
