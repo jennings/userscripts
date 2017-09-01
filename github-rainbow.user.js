e4f72fbed1ca502eb5256d8deb46c732258c2bc6
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-08-31 18:05:46 -0700

    github-rainbow: update .Header class name

--- github-rainbow.user.js
+++ github-rainbow.user.js
@@ -3,7 +3,7 @@
 // @description  Makes the GitHub menu bar more fabulous.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20170809
+// @version      1.20170831
 // @match        https://github.com/*
 // @grant        GM_addStyle
 // @run-at       document-start
@@ -15,38 +15,38 @@
   'use strict';
 
   GM_addStyle(''
-    + '.header {'
+    + '.Header {'
     +   'background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);'
     +   'padding-top: 3px;'
     +   'padding-bottom: 3px;'
     + '}'
 
-    + '.header .container {'
+    + '.Header .container {'
     +   'background-color: rgba(100, 100, 100, 0.5);'
     +   'border-radius: 15px;'
     +   'padding: 5px 10px;'
     + '}'
 
-    + '.header .header-search, .header .header-search-input {'
+    + '.Header .header-search, .header .header-search-input {'
     +   'color: white;'
     +   'text-shadow: 1px 1px 1px black;'
     + '}'
 
-    + '.header .header-search-wrapper, .header .header-search-wrapper.focus {'
+    + '.Header .header-search-wrapper, .header .header-search-wrapper.focus {'
     +   'background-color: rgba(0, 0, 0, 0.15);'
     + '}'
 
-    + '.header .header-search, .header .header-search-input {'
+    + '.Header .header-search, .header .header-search-input {'
     +   'color: white;'
     +   'text-shadow: 1px 1px 1px black;'
     + '}'
 
-    + '.header .header-search-scope, .header .header-search-wrapper.focus .header-search-scope {'
+    + '.Header .header-search-scope, .header .header-search-wrapper.focus .header-search-scope {'
     +   'color: white;'
     +   'border-right-color: rgba(255, 255, 255, 0.75);'
     + '}'
 
-    + '.header .header-navlink {'
+    + '.Header .header-navlink {'
     +   'color: white;'
     +   'text-shadow: 1px 1px 1px black;'
     + '}'
