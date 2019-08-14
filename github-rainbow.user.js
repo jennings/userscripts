ceee604d8381590a3f338aa221e6acfb475091c5
Stephen Jennings <stephen.g.jennings@gmail.com>
2019-08-14 09:50:30 -0700

    github-rainbow: improve legibility by reducing rainbow size

--- github-rainbow.user.js
+++ github-rainbow.user.js
@@ -3,7 +3,7 @@
 // @description  Makes the GitHub menu bar more fabulous.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20180403
+// @version      1.20190814
 // @match        https://github.com/*
 // @run-at       document-start
 // ==/UserScript==
@@ -13,43 +13,22 @@
 (function() {
   'use strict';
 
+  addDivAsFirstChild(document.querySelector('.Header'), 'rainbow');
+
   addStyleBlock(`
-     .Header {
+     .Header > .rainbow {
        background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
-       padding-top: 3px;
-       padding-bottom: 3px;
-     }
-
-     .Header .container {
-       background-color: rgba(100, 100, 100, 0.5);
-       border-radius: 15px;
-       padding: 5px 10px;
-     }
-
-     .Header .header-search, .header .header-search-input {
-       color: white;
-       text-shadow: 1px 1px 1px black;
-     }
-
-     .Header .header-search-wrapper, .header .header-search-wrapper.focus {
-       background-color: rgba(0, 0, 0, 0.20);
-     }
-
-     .Header .header-search, .header .header-search-input {
-       color: white;
-       text-shadow: 1px 1px 1px black;
-     }
-
-     .Header .header-search-scope, .header .header-search-wrapper.focus .header-search-scope {
-       color: white;
-       border-right-color: rgba(255, 255, 255, 0.75);
-     }
-
-     .Header .header-navlink {
-       color: white;
-       text-shadow: 1px 1px 1px black;
+       position: absolute;
+       top: 0; left: 0; right: 0;
+       height: 5px;
      }
   `)
+
+  function addDivAsFirstChild(node, classname) {
+    const div = document.createElement('div');
+    div.classList.add('rainbow');
+    header.prepend(div);
+  }
 
   function addStyleBlock(css) {
     const style = document.createElement('style')
