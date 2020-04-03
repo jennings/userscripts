9ff1605cb4af79db1ec34cb7d2a8432d32aacbee
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-04-03 10:11:34 -0700

    add harness-firefox-fixer

---
+++ harness-firefox-fixer.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Harness Firefox fixer
+// @description  Harness doesn't seem to care about Firefox.
+// @author       Stephen Jennings
+// @namespace    http://jennings.io
+// @version      1.2020040301
+// @match        https://app.harness.io/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  addStyleBlock(`
+    [data-name='pipelineView'] {
+        display: none;
+    }
+  `);
+
+  function addStyleBlock(css) {
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+  }
+})();
