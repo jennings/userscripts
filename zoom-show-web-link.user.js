e7efe5aa55eae4e989db273c37ed9207e5a295db
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-06-22 09:06:56 -0700

    zoom-show-web-link: update

--- zoom-show-web-link.user.js
+++ zoom-show-web-link.user.js
@@ -1,27 +1,38 @@
 // ==UserScript==
 // @name         Zoom show web link
-// @description  Unhides the "Join from your browser" link from the Zoom launch page.
+// @description  Adds a link to the Zoom web client to the join page.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20200429
+// @version      1.20200622.0
 // @match        https://*.zoom.us/j/*
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  addStyleBlock(`
-    .webclient.hideme {
-      display: inherit;
+  function getMeetingIdFromLocation() {
+    const { pathname } = location;
+    const re = /\/j\/(\d+)/;
+    const match = pathname.match(re);
+    if (match) {
+      return match[1];
     }
-  `)
-
-  function addStyleBlock(css) {
-    const style = document.createElement('style')
-    style.type = 'text/css'
-    style.appendChild(document.createTextNode(css))
-    document.head.appendChild(style)
   }
 
+  function getWebClientUrl(meetingId) {
+    return `https://zoom.us/wc/join/${meetingId}`;
+  }
+
+  const id = setTimeout(() => {
+    const meetingId = getMeetingIdFromLocation();
+    const meetingUrl = getWebClientUrl(meetingId);
+
+    const div = document.createElement("div");
+    div.innerHTML = `Web client: <a href="${meetingUrl}">${meetingUrl}</a>`;
+
+    const parentElement =
+      document.querySelector("h3[role=presentation]") || document.body;
+    parentElement.appendChild(div);
+  }, 100);
 })();
