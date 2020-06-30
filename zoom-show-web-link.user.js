ecc31744913f5f6120fe38052663eef7b9b96f4c
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-06-30 08:59:50 -0700

    zoom-show-web-link: retry a couple times before falling back

--- zoom-show-web-link.user.js
+++ zoom-show-web-link.user.js
@@ -8,7 +8,7 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function () {
+(function() {
   "use strict";
 
   function getMeetingIdFromLocation() {
@@ -24,15 +24,22 @@
     return `https://zoom.us/wc/join/${meetingId}`;
   }
 
-  const id = setTimeout(() => {
+  let attempts = 4;
+  const id = setInterval(() => {
     const meetingId = getMeetingIdFromLocation();
     const meetingUrl = getWebClientUrl(meetingId);
 
     const div = document.createElement("div");
     div.innerHTML = `Web client: <a href="${meetingUrl}">${meetingUrl}</a>`;
 
-    const parentElement =
-      document.querySelector("h3[role=presentation]") || document.body;
-    parentElement.appendChild(div);
+    const parentElement = document.querySelector("h3[role=presentation]");
+
+    if (parentElement != null) {
+      parentElement.appendChild(div);
+      clearInterval(id);
+    } else if (attempts-- <= 0) {
+      document.body.appendChild(div);
+      clearInterval(id);
+    }
   }, 100);
 })();
