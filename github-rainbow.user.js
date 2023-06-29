1bcffb5767ba75c521a3358d26f3f70509e0b0f5
Stephen Jennings <stephen.g.jennings@gmail.com>
2023-06-29 15:52:13 -0700

    Update github-rainbow.user.js

--- github-rainbow.user.js
+++ github-rainbow.user.js
@@ -3,7 +3,7 @@
 // @description  Makes the GitHub menu bar more fabulous.
 // @author       Stephen Jennings
 // @namespace    http://jennings.io/
-// @version      1.20191203
+// @version      1.20230629
 // @match        https://github.com/*
 // @run-at       document-start
 // ==/UserScript==
@@ -13,20 +13,15 @@
 (function () {
   "use strict";
 
-  addDivAsFirstChild(document.querySelector(".Header"), "rainbow");
-
-  addStyleBlock(`
-     .Header > .rainbow {
-       background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
-       position: absolute;
-       top: 0; left: 0; right: 0;
-       height: 5px;
-     }
-  `);
+  const root = document.querySelector(".js-header-wrapper").parentElement;
+  addDivAsFirstChild(root, "rainbow");
 
   function addDivAsFirstChild(node, classname) {
     const div = document.createElement("div");
     div.classList.add(classname);
+    div.style.background = "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)";
+    div.style.width = "100%";
+    div.style.height = "2px";
     node.prepend(div);
   }
 
